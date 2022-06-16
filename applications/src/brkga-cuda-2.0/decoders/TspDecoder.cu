0f869b4c60b801914d06b06b93063851375ef2ef
Bruno Almêda de Oliveira <abrunoaa@gmail.com>
2022-06-16 02:30:13 +0000

    Fix TSP GPU decoder

--- applications/src/brkga-cuda-2.0/decoders/TspDecoder.cu
+++ applications/src/brkga-cuda-2.0/decoders/TspDecoder.cu
@@ -14,6 +14,10 @@ TspDecoder::TspDecoder(TspInstance* _instance)
       dDistances(box::cuda::alloc<float>(nullptr, instance->distances.size())) {
   box::cuda::copy_htod(nullptr, dDistances, instance->distances.data(),
                        instance->distances.size());
+
+  // Set CUDA heap limit to 1GB to avoid memory issues with the sort of thrust
+  CUDA_CHECK(cudaDeviceSetLimit(cudaLimitMallocHeapSize,
+                                (std::size_t)1024 * 1024 * 1024));
 }
 
 TspDecoder::~TspDecoder() {
