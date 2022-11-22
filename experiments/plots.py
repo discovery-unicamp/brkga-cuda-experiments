4bbc5581fc9bddaabc566cb90e2af23c85ad7769
Bruno Almêda de Oliveira <abrunoaa@gmail.com>
2022-11-22 16:54:33 -0300

    Join all results to simplify the logic

--- experiments/plots.py
+++ experiments/plots.py
@@ -18,7 +18,8 @@ FILES = [
     'BRKGA-CUDA+pruning.zip',
     'BRKGA-CUDA.zip',
 ]
-DATA = {file: read_results(Path(f'results/{file}')) for file in FILES}
+data = pd.concat((read_results(Path(f'results/{file}')) for file in FILES),
+                 ignore_index=True)
 
 COLORS = ['red', 'green', 'blue', 'purple']
 
@@ -33,58 +34,47 @@ PLOT_GENERATIONS = False
 
 
 def plot_convergence():
-    df = DATA[FILES[0]]
-    instances = df[['problem', 'instance']].drop_duplicates()
-    # instances = instances[instances['instance'].isin(['X-n219-k73'])]
-    color_label = {}
-
-    plot_params = list(PARAMS - {'problem', 'instance', 'seed'})
-
-    for _, inst in instances.iterrows():
+    plot_by = ['problem', 'instance']
+    plot_params = list(PARAMS - {'seed'} - set(plot_by))
+    label_color = {}
+    for _, plot_condition in data[plot_by].drop_duplicates().iterrows():
         plt.figure(figsize=(10.80, 7.20))
-        problem = inst['problem']
-        instance = inst['instance']
 
-        for input_file in FILES:
-            df = DATA[input_file]
-            df = df.loc[(df['instance'] == instance)]
-            df = df.loc[(df['problem'] == problem)]
-            df = build_apc(df)
+        mask = True
+        for p in plot_by:
+            mask = mask & (data[p] == plot_condition[p])
+        df = build_apc(data.loc[mask])
 
-            for _, row in df.iterrows():
-                fitness, elapsed, generation = zip(*row['convergence'])
-                if not SMOOTH_PLOT:
-                    fitness = [y
-                               for i, x in enumerate(fitness)
-                               for y in (1 if i == len(fitness) - 1 else 2)
-                                        * [x]]
-                    elapsed = [y
-                               for i, x in enumerate(elapsed)
-                               for y in (1 if i == 0 else 2) * [x]]
-                    generation = [y
-                                  for i, x in enumerate(generation)
-                                  for y in (1 if i == 0 else 2) * [x]]
+        for _, row in df.iterrows():
+            fitness, elapsed, generation = zip(*row['convergence'])
+            if not SMOOTH_PLOT:
+                fitness = [y
+                           for i, x in enumerate(fitness)
+                           for y in (1 if i == len(fitness) - 1 else 2) * [x]]
+                elapsed = [y
+                           for i, x in enumerate(elapsed)
+                           for y in (1 if i == 0 else 2) * [x]]
+                generation = [y
+                              for i, x in enumerate(generation)
+                              for y in (1 if i == 0 else 2) * [x]]
 
-                # if not PLOT_GENERATIONS and elapsed[-1] < row['max-time']:
-                #     elapsed.append(row['max-time'])
-                #     fitness.append(fitness[-1])
+            # if not PLOT_GENERATIONS and elapsed[-1] < row['max-time']:
+            #     elapsed.append(row['max-time'])
+            #     fitness.append(fitness[-1])
 
-                label = '_'.join(map(str, row[plot_params]))
-                if label not in color_label:
-                    assert COLORS
-                    color_label[label] = COLORS.pop()
+            label = '_'.join(map(str, row[plot_params]))
+            if label not in label_color:
+                assert COLORS
+                label_color[label] = COLORS.pop()
 
-                plt.plot(
-                    (generation if PLOT_GENERATIONS else elapsed),
-                    fitness,
-                    label=label,
-                    color=color_label[label],
-                )
+            plt.plot(
+                (generation if PLOT_GENERATIONS else elapsed),
+                fitness,
+                label=label,
+                color=label_color[label],
+            )
 
-        name = f"{problem}-{instance}"
-
-        if problem == 'scp':
-            plt.ylim((500, 1500))
+        name = '-'.join(plot_condition[p] for p in plot_by)
 
         plt.title(f"{name} convergence")
         plt.xlabel("Generation" if PLOT_GENERATIONS else "Time (s)")
@@ -93,7 +83,8 @@ def plot_convergence():
         plt.minorticks_on()
         plt.grid(axis='y', which='minor', linestyle=':')
         plt.grid(axis='y', which='major', linestyle='-')
-        plt.grid(axis='x', which='major', linestyle=':')
+        plt.grid(axis='x', which='major', linestyle='--')
+        plt.grid(axis='x', which='minor', linestyle=':')
 
         outfile = Path('convergence', name + ".png")
         outfile.parent.mkdir(parents=True, exist_ok=True)
@@ -120,9 +111,13 @@ def build_apc(df: pd.DataFrame) -> pd.DataFrame:
 
         return apc
 
-    df = df.groupby(list(PARAMS - {'seed'})).apply(eval_apc).reset_index()
-    df = df.rename(columns={0: 'convergence'})
-    return df
+    return (
+        df
+        .groupby(list(PARAMS - {'seed'}))
+        .apply(eval_apc)
+        .rename('convergence')
+        .reset_index()
+    )
 
 
 def find_conv(conv: List[Tuple[float, float, int]], elapsed: float):
